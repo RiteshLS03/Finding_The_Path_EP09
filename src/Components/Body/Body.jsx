96755f20c295375e744fbe354266ac792ff1097c
Ritesh Narwade <riteshnarwade03@gmail.com>
2023-09-09 20:16:12 +0530

    Day02

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,8 +1,43 @@
-import { RestaurantCard } from "../Index";
+import { useEffect,useState } from "react";
+import { RestaurantCard , ShimmarUI} from "../Index";
+import { SwiggyAPI_URL } from "../config.js";
 import "./Body.css"
 
-function Body ({filteredRestaurants}) {
-    return (
+function Body () {
+  const [allRestaurants , setAllRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  useEffect(()=>{
+    getResturants()
+  },[])
+
+
+  async function  getResturants(){
+
+    try {
+      const response = await fetch(SwiggyAPI_URL);
+      const json = await response.json();
+
+      async function checkJsonData(jsondata){
+        for(i=0;i < jsondata?.data?.cards.length; i++){
+
+          let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+          if(checkData !== undefined){
+            return checkData;
+          }
+        }
+      }
+
+      const resData = await checkJsonData(json);
+      setAllRestaurants(resData);
+      setFilteredRestaurants(resData);
+
+    } catch (error) {
+      console.log(error)
+    }
+  } 
+
+  return (allRestaurants.length === 0) ? <ShimmarUI /> :  (
       <div className="body-rest">
       {filteredRestaurants?.map((restaurant) => {
         return   <RestaurantCard restaurant={...restaurant} key={restaurant?.info?.id} /*key={restaurant.data.data.id}*/ /> 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,56 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { logo, vector } from "../../Images/index"; /* Images for UI */
 import "./Header.css";
-import {ShimmarUI} from "../Index";
-import { SwiggyAPI_URL } from "../config.js";
 import {Link} from "react-router-dom";
-import Body from "../Body/Body";
-
-
-function filterData(searchText, allRestaurants) {
-  const filterData = allRestaurants.filter((allRestaurants) =>
-    allRestaurants?.info?.name?.toLowerCase()?.includes(searchText.toLowerCase())
-  );
-  return filterData;
-}
 
 
 function Header () {
-  const [allRestaurants, setAllRestaurants] = useState([])
-  const [searchText, setSearchText] = useState(""); //useState is a function that return an array. First Element is state varible and second element is function that how we want to change the state
-  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
-  console.log(filteredRestaurants);
   const [isLoggedIn,setIsLoggedIn] = useState("true");
-
-  useEffect(()=>{
-    getResturants();
-  },[])
-
-  async function  getResturants(){
-
-    try {
-      const response = await fetch(SwiggyAPI_URL);
-      const json = await response.json();
-
-      async function checkJsonData(jsondata){
-        for(i=0;i < jsondata?.data?.cards.length; i++){
-
-          let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-          if(checkData !== undefined){
-            return checkData;
-          }
-        }
-      }
-
-      const resData = await checkJsonData(json);
-      setAllRestaurants(resData);
-      setFilteredRestaurants(resData);
-
-    } catch (error) {
-      console.log(error)
-    }
-  } 
-
+  const [searchText, setSearchText] = useState(""); //useState is a function that return an array. First Element is state varible and second element is function that how we want to change the state
   return (
     <>
       {/* LOGO */}
@@ -64,14 +20,15 @@ function Header () {
             placeholder="Search, Order, Enjoy!"
             value={searchText}
             onChange={(e) => {
-              setSearchText(e.target.value);
+            var searchingData =  setSearchText(e.target.value);
             }}
           />
           <button 
             onClick={() => {
               // need to filter the data
-              const data = filterData(searchText, allRestaurants);
-              setFilteredRestaurants(data);
+              // const data = filterData(searchText, allRestaurants);
+              // setFilteredRestaurants(data);
+              <Body searchText={searchText} allRestaurants={searchingData} />
             }}
           >
             Search
@@ -94,15 +51,6 @@ function Header () {
             }</ul>    
         </li>
       </div>
-
-      {/* { 
-      
-      (allRestaurants.length === 0) ? <ShimmarUI/> : <Body filteredRestaurants={filteredRestaurants}/>
-
-       } */}
-
-       <Body filteredRestaurants={filteredRestaurants} allRestaurants={allRestaurants} />
-      
   </> );
 }
 
